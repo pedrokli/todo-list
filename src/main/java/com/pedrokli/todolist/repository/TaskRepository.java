97f995f987d4b3b1912ec8904e61d37ea3cc3716
Pedro Klimaitis <pedro.klimaitis@gmail.com>
2023-11-29 18:52:05 -0300

    atualização do Get

--- src/main/java/com/pedrokli/todolist/repository/TaskRepository.java
+++ src/main/java/com/pedrokli/todolist/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.pedrokli.todolist.repository;
 
-public interface TaskRepository {
+import com.pedrokli.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
--- src/main/java/com/pedrokli/todolist/service/TaskService.java
+++ src/main/java/com/pedrokli/todolist/service/TaskService.java
@@ -1,4 +1,31 @@
 package com.pedrokli.todolist.service;
 
+import com.pedrokli.todolist.model.Task;
+import com.pedrokli.todolist.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> listAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public ResponseEntity<Task> findTaskById(Long id){
+        return taskRepository.findById(id)
+                .map(task -> ResponseEntity.ok().body(task))
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
